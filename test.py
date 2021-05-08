9d4384e2c34ff14c98be5d224f933d59d3dd1d64
Hakama555 <54787616+Hakama555@users.noreply.github.com>
2021-05-09 08:56:48 +0900

    make file

---
+++ test.py
@@ -0,0 +1,17 @@
+def prime_num(var):
+    import numpy as np
+    n = int(np.sqrt(var))  # エラストテネスのふるいよりその数の√を求める
+    array = np.ones(var + 1, dtype = "int8")  # np.onceで１が欲しい数個ある配列を作る
+    for an_1 in range(2, var + 1):  # 配列の中で素数で無い物を０に置き換える
+        for an_2 in range(an_1 + an_1, var + 1, an_1):  # 
+            array[an_2] = 0
+    count = 0
+    for j in array:
+        if j == 1:
+            count += 1
+    print(count)
+var = 1000  # 欲しい数字の上限
+prime_num(var)
+# for j in range(2, n + 1):
+#    for k in list:
+        
